f12d713459a947fe90035913705c59c3915407f1
David Salazar <davidsalazar200@gmail.com>
2024-12-06 15:41:52 -0500

    Created checked onChanged functions

--- packages/desktop-client/src/components/settings/Categorization.tsx
+++ packages/desktop-client/src/components/settings/Categorization.tsx
@@ -1,31 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { t } from 'i18next';
-
 import { Text } from '../common/Text';
+import { Block } from '../common/Block';
 import { CheckboxOption } from '../modals/ImportTransactionsModal/CheckboxOption';
+import { theme } from '../../style';
 import { useCategories } from '../../hooks/useCategories';
+import { Button, ButtonWithLoading } from '../common/Button2';
 
 import { Setting } from './UI';
 
 export function Categorization() {
-    const { grouped: categoryGroups } = useCategories();
+  const { grouped: categoryGroups } = useCategories();
+  const [checkedCategories, setCheckedCategories] = useState<string[]>([])
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-    return (
-        <Setting>
-            <Text>
-                <strong>Fuzzy Categorizaiton</strong> allow Fuzzy Finder to automatically categorize transactions based on your history based payee description
-            </Text>
-            {categoryGroups.map((group, groupIndex) => (
-                group.categories?.map((cat, catIndex) => (
-                    <CheckboxOption
-                        id={cat.id}
-                        key={`${groupIndex}-${catIndex}`}
-                    >
-                        {t(cat.name)}
-                    </CheckboxOption>
-                ))
-            ))}
-        </Setting >
-    );
+  const handleCheckboxChange = (categoryName: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = event.target
+    if (checked) {
+      setCheckedCategories([...checkedCategories, categoryName])
+    } else {
+      setCheckedCategories(checkedCategories.filter(item => item !== categoryName));
+    }
+  }
+
+  return (
+    <Setting
+      primaryAction={
+        <>
+          <ButtonWithLoading isLoading={isLoading}>
+            {t('Upload data')}
+          </ButtonWithLoading>
+          {error && (
+            <Block style={{ color: theme.errorText, marginTop: 15 }}>
+              {t(
+                'An unknown error occurred while exporting. Please report this as a new issue on Github.',
+              )}
+            </Block>
+          )}
+        </>
+      }
+    >
+      <Text>
+        <strong>Fuzzy Categorizaiton</strong> allow Fuzzy Finder to automatically categorize transactions based on your history based payee description
+      </Text>
+      <Text>
+        Select the categories you would like to be categorized by Fuzzy Finder:
+      </Text>
+      {categoryGroups.map((group, groupIndex) => (
+        group.categories?.map((cat, catIndex) => (
+          <CheckboxOption
+            id={cat.id}
+            key={`${groupIndex}-${catIndex}`}
+            onChange={handleCheckboxChange(cat.name)}
+          >
+            {t(cat.name)}
+          </CheckboxOption>
+        ))
+      ))}
+    </Setting >
+  );
 }
