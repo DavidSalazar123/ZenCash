a7ed688d6a7bd2c89490b3b49382b06bd23cd44f
David Salazar <davidsalazar200@gmail.com>
2024-12-07 09:33:51 -0500

    Set up elasticsearch node

--- packages/desktop-client/src/components/settings/Categorization.tsx
+++ packages/desktop-client/src/components/settings/Categorization.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import { t } from 'i18next';
 import { Text } from '../common/Text';
@@ -6,15 +6,37 @@ import { Block } from '../common/Block';
 import { CheckboxOption } from '../modals/ImportTransactionsModal/CheckboxOption';
 import { theme } from '../../style';
 import { useCategories } from '../../hooks/useCategories';
-import { Button, ButtonWithLoading } from '../common/Button2';
+import { ButtonWithLoading } from '../common/Button2';
+import {
+  useTransactions,
+} from 'loot-core/client/data-hooks/transactions';
+import * as queries from 'loot-core/client/queries';
 
 import { Setting } from './UI';
+
+/* OPEN ACTIONS
+ * 1) How often do we want to index the fuzzy finder? Should it be automatic? What if I delete a transaction
+ * 2)
+ */
 
 export function Categorization() {
   const { grouped: categoryGroups } = useCategories();
   const [checkedCategories, setCheckedCategories] = useState<string[]>([])
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+
+  // Collect transactions
+  const baseTransactionsQuery = useCallback(
+    () =>
+      queries.transactions().options({ splits: 'none' }).select('*'),
+    [],
+  );
+  const [transactionsQuery, setTransactionsQuery] = useState(
+    baseTransactionsQuery(),
+  );
+  const transactions = useTransactions({
+    query: transactionsQuery,
+  });
 
   const handleCheckboxChange = (categoryName: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
     const { checked } = event.target
@@ -25,12 +47,18 @@ export function Categorization() {
     }
   }
 
+  async function uploadTransactions() {
+
+  }
+
+
+
   return (
     <Setting
       primaryAction={
         <>
-          <ButtonWithLoading isLoading={isLoading}>
-            {t('Upload data')}
+          <ButtonWithLoading onPress={uploadTransactions} isLoading={isLoading}>
+            {t('Save')}
           </ButtonWithLoading>
           {error && (
             <Block style={{ color: theme.errorText, marginTop: 15 }}>
@@ -46,7 +74,7 @@ export function Categorization() {
         <strong>Fuzzy Categorizaiton</strong> allow Fuzzy Finder to automatically categorize transactions based on your history based payee description
       </Text>
       <Text>
-        Select the categories you would like to be categorized by Fuzzy Finder:
+        Select the categories you would like to be automatically categorized by Fuzzy Finder:
       </Text>
       {categoryGroups.map((group, groupIndex) => (
         group.categories?.map((cat, catIndex) => (
--- packages/desktop-client/src/components/settings/index.tsx
+++ packages/desktop-client/src/components/settings/index.tsx
@@ -25,6 +25,7 @@ import { useServerVersion } from '../ServerContext';
 
 import { Backups } from './Backups';
 import { BudgetTypeSettings } from './BudgetTypeSettings';
+import { Categorization } from './Categorization';
 import { EncryptionSettings } from './Encryption';
 import { ExperimentalFeatures } from './Experimental';
 import { ExportBudget } from './Export';
@@ -35,158 +36,159 @@ import { ThemeSettings } from './Themes';
 import { AdvancedToggle, Setting } from './UI';
 
 function About() {
-  const version = useServerVersion();
-  const latestVersion = useLatestVersion();
-  const isOutdated = useIsOutdated();
+    const version = useServerVersion();
+    const latestVersion = useLatestVersion();
+    const isOutdated = useIsOutdated();
 
-  return (
-    <Setting>
-      <Text>
-        <strong>{t('Actual')}</strong>
-        {t(' is a super fast privacy-focused app for managing your finances.')}
-      </Text>
-      <View
-        style={{
-          flexDirection: 'column',
-          gap: 10,
-        }}
-        className={css({
-          [`@media (min-width: ${tokens.breakpoint_small})`]: {
-            display: 'grid',
-            gridTemplateRows: '1fr 1fr',
-            gridTemplateColumns: '50% 50%',
-            columnGap: '2em',
-            gridAutoFlow: 'column',
-          },
-        })}
-        data-vrt-mask
-      >
-        <Text>Client version: v{window.Actual?.ACTUAL_VERSION}</Text>
-        <Text>Server version: {version}</Text>
-        {isOutdated ? (
-          <Link
-            variant="external"
-            to="https://actualbudget.org/docs/releases"
-            linkColor="purple"
-          >
-            {t('New version available:')} {latestVersion}
-          </Link>
-        ) : (
-          <Text style={{ color: theme.noticeText, fontWeight: 600 }}>
-            {t('You’re up to date!')}
-          </Text>
-        )}
-        <Text>
-          <Link
-            variant="external"
-            to="https://actualbudget.org/docs/releases"
-            linkColor="purple"
-          >
-            {t('Release Notes')}
-          </Link>
-        </Text>
-      </View>
-    </Setting>
-  );
+    return (
+        <Setting>
+            <Text>
+                <strong>{t('Actual')}</strong>
+                {t(' is a super fast privacy-focused app for managing your finances.')}
+            </Text>
+            <View
+                style={{
+                    flexDirection: 'column',
+                    gap: 10,
+                }}
+                className={css({
+                    [`@media (min-width: ${tokens.breakpoint_small})`]: {
+                        display: 'grid',
+                        gridTemplateRows: '1fr 1fr',
+                        gridTemplateColumns: '50% 50%',
+                        columnGap: '2em',
+                        gridAutoFlow: 'column',
+                    },
+                })}
+                data-vrt-mask
+            >
+                <Text>Client version: v{window.Actual?.ACTUAL_VERSION}</Text>
+                <Text>Server version: {version}</Text>
+                {isOutdated ? (
+                    <Link
+                        variant="external"
+                        to="https://actualbudget.org/docs/releases"
+                        linkColor="purple"
+                    >
+                        {t('New version available:')} {latestVersion}
+                    </Link>
+                ) : (
+                    <Text style={{ color: theme.noticeText, fontWeight: 600 }}>
+                        {t('You’re up to date!')}
+                    </Text>
+                )}
+                <Text>
+                    <Link
+                        variant="external"
+                        to="https://actualbudget.org/docs/releases"
+                        linkColor="purple"
+                    >
+                        {t('Release Notes')}
+                    </Link>
+                </Text>
+            </View>
+        </Setting>
+    );
 }
 
 function IDName({ children }: { children: ReactNode }) {
-  return <Text style={{ fontWeight: 500 }}>{children}</Text>;
+    return <Text style={{ fontWeight: 500 }}>{children}</Text>;
 }
 
 function AdvancedAbout() {
-  const [budgetId] = useMetadataPref('id');
-  const [groupId] = useMetadataPref('groupId');
+    const [budgetId] = useMetadataPref('id');
+    const [groupId] = useMetadataPref('groupId');
 
-  return (
-    <Setting>
-      <Text>
-        <strong>{t('IDs')}</strong>
-        {t(
-          ' are the names Actual uses to identify your budget internally. There are several different IDs associated with your budget. The Budget ID is used to identify your budget file. The Sync ID is used to access the budget on the server.',
-        )}
-      </Text>
-      <Text>
-        <IDName>{t('Budget ID:')}</IDName> {budgetId}
-      </Text>
-      <Text style={{ color: theme.pageText }}>
-        <IDName>{t('Sync ID:')}</IDName> {groupId || '(none)'}
-      </Text>
-      {/* low priority todo: eliminate some or all of these, or decide when/if to show them */}
-      {/* <Text>
+    return (
+        <Setting>
+            <Text>
+                <strong>{t('IDs')}</strong>
+                {t(
+                    ' are the names Actual uses to identify your budget internally. There are several different IDs associated with your budget. The Budget ID is used to identify your budget file. The Sync ID is used to access the budget on the server.',
+                )}
+            </Text>
+            <Text>
+                <IDName>{t('Budget ID:')}</IDName> {budgetId}
+            </Text>
+            <Text style={{ color: theme.pageText }}>
+                <IDName>{t('Sync ID:')}</IDName> {groupId || '(none)'}
+            </Text>
+            {/* low priority todo: eliminate some or all of these, or decide when/if to show them */}
+            {/* <Text>
         <IDName>Cloud File ID:</IDName> {prefs.cloudFileId || '(none)'}
       </Text>
       <Text>
         <IDName>User ID:</IDName> {prefs.userId || '(none)'}
       </Text> */}
-    </Setting>
-  );
+        </Setting>
+    );
 }
 
 export function Settings() {
-  const [floatingSidebar] = useGlobalPref('floatingSidebar');
-  const [budgetName] = useMetadataPref('budgetName');
+    const [floatingSidebar] = useGlobalPref('floatingSidebar');
+    const [budgetName] = useMetadataPref('budgetName');
 
-  const { loadPrefs, closeBudget } = useActions();
+    const { loadPrefs, closeBudget } = useActions();
 
-  useEffect(() => {
-    const unlisten = listen('prefs-updated', () => {
-      loadPrefs();
-    });
+    useEffect(() => {
+        const unlisten = listen('prefs-updated', () => {
+            loadPrefs();
+        });
 
-    loadPrefs();
-    return () => unlisten();
-  }, [loadPrefs]);
+        loadPrefs();
+        return () => unlisten();
+    }, [loadPrefs]);
 
-  const { isNarrowWidth } = useResponsive();
+    const { isNarrowWidth } = useResponsive();
 
-  return (
-    <Page
-      header={t('Settings')}
-      style={{
-        marginInline: floatingSidebar && !isNarrowWidth ? 'auto' : 0,
-        paddingBottom: MOBILE_NAV_HEIGHT,
-      }}
-    >
-      <View
-        style={{
-          marginTop: 10,
-          flexShrink: 0,
-          maxWidth: 530,
-          gap: 30,
-        }}
-      >
-        {isNarrowWidth && (
-          <View
-            style={{ gap: 10, flexDirection: 'row', alignItems: 'flex-end' }}
-          >
-            {/* The only spot to close a budget on mobile */}
-            <FormField>
-              <FormLabel title={t('Budget Name')} />
-              <Input
-                value={budgetName}
-                disabled
-                style={{ color: theme.buttonNormalDisabledText }}
-              />
-            </FormField>
-            <Button onPress={closeBudget}>{t('Close Budget')}</Button>
-          </View>
-        )}
-        <About />
-        <ThemeSettings />
-        <FormatSettings />
-        <EncryptionSettings />
-        <BudgetTypeSettings />
-        {isElectron() && <Backups />}
-        <ExportBudget />
-        <AdvancedToggle>
-          <AdvancedAbout />
-          <ResetCache />
-          <ResetSync />
-          <FixSplits />
-          <ExperimentalFeatures />
-        </AdvancedToggle>
-      </View>
-    </Page>
-  );
+    return (
+        <Page
+            header={t('Settings')}
+            style={{
+                marginInline: floatingSidebar && !isNarrowWidth ? 'auto' : 0,
+                paddingBottom: MOBILE_NAV_HEIGHT,
+            }}
+        >
+            <View
+                style={{
+                    marginTop: 10,
+                    flexShrink: 0,
+                    maxWidth: 530,
+                    gap: 30,
+                }}
+            >
+                {isNarrowWidth && (
+                    <View
+                        style={{ gap: 10, flexDirection: 'row', alignItems: 'flex-end' }}
+                    >
+                        {/* The only spot to close a budget on mobile */}
+                        <FormField>
+                            <FormLabel title={t('Budget Name')} />
+                            <Input
+                                value={budgetName}
+                                disabled
+                                style={{ color: theme.buttonNormalDisabledText }}
+                            />
+                        </FormField>
+                        <Button onPress={closeBudget}>{t('Close Budget')}</Button>
+                    </View>
+                )}
+                <About />
+                <ThemeSettings />
+                <FormatSettings />
+                <EncryptionSettings />
+                <BudgetTypeSettings />
+                {isElectron() && <Backups />}
+                <ExportBudget />
+                <Categorization />
+                <AdvancedToggle>
+                    <AdvancedAbout />
+                    <ResetCache />
+                    <ResetSync />
+                    <FixSplits />
+                    <ExperimentalFeatures />
+                </AdvancedToggle>
+            </View>
+        </Page>
+    );
 }
